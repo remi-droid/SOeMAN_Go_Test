344b8b33ce3c80f3ed8d7eda334951c41c0e75d0
regiraudon <Remi.GIRAUDON@etu.uca.fr>
2024-11-26 11:17:22 +0000

    Ajout de l'objet Document et du serveur Gin.

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Document struct {
+	ID         int       `json:"id"`
+	Name       string    `json:"name"`
+	Url        string    `json:"url"`
+	UploadDate time.Time `json:"uploaded_at"`
+}
 
 // The DSN to connect to postgres is: "postgres://upload-service:password@postgres:5432/main".
 
 func main() {
-	fmt.Println("Hello, world!")
+
+	r := gin.Default()
+
+	// Routes
+
+	// The servers runs on port 8080
+	r.Run(":8080")
+
 }
